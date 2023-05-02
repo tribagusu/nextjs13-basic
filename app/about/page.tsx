2d2293b0783bf33afbe4a5d671dfbf81303ee2c7
tribagus <tribagus.indonesia@gmail.com>
2023-05-02 14:39:48 +0700

    create first page

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -1,5 +1,17 @@
 import React from "react";
+import Link from "next/link";
+import { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: "About",
+  description: "This is about page",
+};
 
 export default function About() {
-  return <div>About</div>;
+  return (
+    <>
+      <h1>About</h1>
+      <Link href="/">Go to Home Page</Link>
+    </>
+  );
 }
